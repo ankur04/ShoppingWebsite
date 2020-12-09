aae47fac1f0bd1449b55606824a5dfe8295031af
ankur04 <ankurpravinpatel@gmail.com>
2020-12-10 01:22:57 +0530

    added focus for safari with another way

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -74,7 +74,7 @@ class SignIn extends React.Component {
               type="password"
               ref={this.passwordInput}
               readOnly={this.state.disablePassword}
-              onFocus={setTimeout(this.handleOnFocusPassword, 1)}
+              onFocus={this.handleOnFocusPassword}
               onBlur={this.handleOnBlurPassword}
               value={this.state.password}
               onChange={this.handleChange}
