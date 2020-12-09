5522dc08951db595a1c7a8407361bc1b77c0ff42
ankur04 <ankurpravinpatel@gmail.com>
2020-12-09 23:34:45 +0530

    changed on focus for ios compatibility

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,36 +1,50 @@
-import React from 'react';
+import React from "react";
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
-import './sign-in.styles.scss';
+import "./../form-input/form-input.styles.scss";
+import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: '',
-      password: ''
+      email: "",
+      password: "",
+      disablePassword: true,
     };
   }
 
-  handleSubmit = async event => {
+  handleOnFocusPassword = () => {
+    this.setState({
+      disablePassword: false,
+    });
+  };
+
+  handleOnBlurPassword = () => {
+    this.setState({
+      disablePassword: true,
+    });
+  };
+
+  handleSubmit = async (event) => {
     event.preventDefault();
 
     const { email, password } = this.state;
 
     try {
       await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
+      this.setState({ email: "", password: "" });
     } catch (error) {
       console.log(error);
     }
   };
 
-  handleChange = event => {
+  handleChange = (event) => {
     const { value, name } = event.target;
 
     this.setState({ [name]: value });
@@ -38,30 +52,50 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className='sign-in'>
+      <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            name='email'
-            type='email'
+            name="email"
+            type="email"
             handleChange={this.handleChange}
             value={this.state.email}
-            label='email'
+            label="email"
             required
           />
-          <FormInput
-            name='password'
-            type='password'
-            value={this.state.password}
-            handleChange={this.handleChange}
-            label='password'
-            required
-          />
-          <div className='buttons'>
-            <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
+
+          <div className="group">
+            <input
+              className="form-input"
+              name="password"
+              type="password"
+              readOnly={this.state.disablePassword}
+              onFocus={this.handleOnFocusPassword}
+              onBlur={this.handleOnBlurPassword}
+              value={this.state.password}
+              onChange={this.handleChange}
+              label="password"
+              required
+            />
+            {"Password" ? (
+              <label
+                className={`${
+                  this.state.password.length ? "shrink" : ""
+                } form-input-label`}
+              >
+                {"Password"}
+              </label>
+            ) : null}
+          </div>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </div>
